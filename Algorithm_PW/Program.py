4c8bdaac95c880c8694a798f6ad3aeb4d03bf554
Sreehari Premkumar <44517540+xHotshot@users.noreply.github.com>
2020-11-27 20:33:18 +0530

    Update Program.py

--- Algorithm_PW/Program.py
+++ Algorithm_PW/Program.py
@@ -1,146 +1,189 @@
-#importing the necessary libraries
-import glob	    
-import cv2
+import sys
+import glob
 import os
-import shutil
-import time
-
-#Quick sort Algorithm to sort the images based on size
-def QuickSort(arr,left,right):
-    i = left
-    j = right
-    piv = arr[left + (right - left) // 2][1] 
-    while i <= j:
-        while arr[i][1] < piv: i += 1
-        while arr[j][1] > piv: j -= 1
-        if i <= j: 
-            arr[i], arr[j] = arr[j], arr[i]
-            i += 1
-            j -= 1
-    if left < j:
-        QuickSort(arr, left, j)
-    if i < right: 
-        QuickSort(arr, i, right)
-    return arr
-
-#Sort Function
-def Sort(arr,n):
-    begin = time.perf_counter()
-    arr1 = QuickSort(arr,0,n-1)
-    end = time.perf_counter()
-
-    print(f"\nTotal runtime of the program is {(end - begin)*1000000000} ns")
-
-    if os.path.exists(dir+'/Sorted'):
-        shutil.rmtree(dir+'/Sorted')
-    os.makedirs(dir+'/Sorted')
-
-    for i in range(n):
-        shutil.copy(dir+'/Images/'+arr1[i][0],dir+'/Sorted')
-        os.rename(dir+'/Sorted/'+arr1[i][0],dir+'/Sorted/{0} '.format(i)+arr1[i][0])
-    return
-
-#Binary search function
-def BinarySearch(arr,img_size):
-
-    h = n-1
-    l = 0
-    m = 0
-    arr = QuickSort(arr,0,n-1)
-    begin = time.perf_counter()
-    while l<=h:
-        
-        m = int((l+h)/2)
-
-        if arr[m][1] < img_size:
-            l = m + 1
-        elif arr[m][1] > img_size:
-            h = m - 1
-        else:
-            end =time.perf_counter()
-            print(f"\nTotal runtime of the program is {(end - begin)*1000000000} ns")
-            return m,arr
-    end =time.perf_counter()
-    print(f"\nTotal runtime of the program is {(end - begin)*1000000000} ns")
-    return -1,arr
-
-#Linear search function
-def LinearSearch(arr,img_size,img):
-    begin = time.perf_counter()
-
-    for i in range(n):
-        if(arr[i][1]==img_size):
-            img2 = cv2.imread(dir+"/Images/"+arr[i][0])
-
-            if(img == img2).all():
-                end =time.perf_counter()
-                print(f"\nTotal runtime of the program is {(end - begin)*1000000000} ns")
-                return i
-            
-    end = time.perf_counter()
-
-    print(f"\nTotal runtime of the program is {(end - begin)*1000000000} ns")
-    return -1
+import glob
+import numpy as np
+import cv2
+from PIL import Image
+import pytesseract
+import re
 
 
-#Search Function
-def Search(arr,dir):
-
-    files=glob.glob(dir+"/Finder/*.jpg")
-
-    if not files:
-        print("No images in the finder folder")
-        return
-    img = cv2.imread(files[0])
-    img_size = img.size
-    out = 0
-
-    sw = int(input("\n\n1) Linear. \n2) Binary\nEnter : "))
-    if sw == 1 :
-        out = LinearSearch(arr,img_size,img)
-    elif sw == 2 :
-        out,arr = BinarySearch(arr,img_size)
-    else:
-        print("\n\nInvalid Choice")
-        return
-
-    if out == -1:
-        print("\nImage Not Found")
-    else:
-
-        img2 = cv2.imread(dir+"/Images/"+arr[out][0])
-
-        img = cv2.resize(img, (960, 960))
-        img2 = cv2.resize(img2, (960, 960))
-        cv2.imshow("Image 1",img)
-        cv2.imshow("Image 2",img2)
-        cv2.waitKey(0)
+def number_plate_detection(img):
+    def clean2_plate(plate):
+        gray_img = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
     
-    return
+        _, thresh = cv2.threshold(gray_img, 110, 255, cv2.THRESH_BINARY)
+        if cv2.waitKey(0) & 0xff == ord('q'):
+            pass
+        num_contours,hierarchy = cv2.findContours(thresh.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    
+        if num_contours:
+            contour_area = [cv2.contourArea(c) for c in num_contours]
+            max_cntr_index = np.argmax(contour_area)
+    
+            max_cnt = num_contours[max_cntr_index]
+            max_cntArea = contour_area[max_cntr_index]
+            x,y,w,h = cv2.boundingRect(max_cnt)
+    
+            if not ratioCheck(max_cntArea,w,h):
+                return plate,None
+    
+            final_img = thresh[y:y+h, x:x+w]
+            return final_img,[x,y,w,h]
+    
+        else:
+            return plate,None
+    
+    def ratioCheck(area, width, height):
+        ratio = float(width) / float(height)
+        if ratio < 1:
+            ratio = 1 / ratio
+        if (area < 1063.62 or area > 73862.5) or (ratio < 3 or ratio > 6):
+            return False
+        return True
+    
+    def isMaxWhite(plate):
+        avg = np.mean(plate)
+        if(avg>=115):
+            return True
+        else:
+            return False
+    
+    def ratio_and_rotation(rect):
+        (x, y), (width, height), rect_angle = rect
+    
+        if(width>height):
+            angle = -rect_angle
+        else:
+            angle = 90 + rect_angle
+    
+        if angle>15:
+            return False
+    
+        if height == 0 or width == 0:
+            return False
+    
+        area = height*width
+        if not ratioCheck(area,width,height):
+            return False
+        else:
+            return True
+    
+    
+    img2 = cv2.GaussianBlur(img, (5,5), 0)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+    
+    img2 = cv2.Sobel(img2,cv2.CV_8U,1,0,ksize=3)	
+    _,img2 = cv2.threshold(img2,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    
+    element = cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=(17, 3))
+    morph_img_threshold = img2.copy()
+    cv2.morphologyEx(src=img2, op=cv2.MORPH_CLOSE, kernel=element, dst=morph_img_threshold)
+    num_contours, hierarchy= cv2.findContours(morph_img_threshold,mode=cv2.RETR_EXTERNAL,method=cv2.CHAIN_APPROX_NONE)
+    cv2.drawContours(img2, num_contours, -1, (0,255,0), 1)
+    
+    
+    for i,cnt in enumerate(num_contours):
+        min_rect = cv2.minAreaRect(cnt)
+        if ratio_and_rotation(min_rect):
+            x,y,w,h = cv2.boundingRect(cnt)
+            plate_img = img[y:y+h,x:x+w]
+            if(isMaxWhite(plate_img)):
+                clean_plate, rect = clean2_plate(plate_img)
+                if rect:
+                    fg=0
+                    x1,y1,w1,h1 = rect
+                    x,y,w,h = x+x1,y+y1,w1,h1
+                    plate_im = Image.fromarray(clean_plate)
+                    text = pytesseract.image_to_string(plate_im, lang='eng')
+                    return text
+                    
+                    
+    
 
-#Read the images from Images folder
-def Read(dir):
 
-    files = glob.glob(dir+'/Images/*.jpg')
-    n = len(files)
-    x_data =[]
+def partition(arr,low,high): 
+    i = ( low-1 )         
+    pivot = arr[high]    
+  
+    for j in range(low , high): 
+        if   arr[j] < pivot: 
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+  
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 ) 
 
-    for i in files:
-        img = cv2.imread(i)
-        x_data.append([os.path.basename(i),img.size])
+def quickSort(arr,low,high): 
+    if low < high: 
+        pi = partition(arr,low,high) 
+  
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high)
+        
+    return arr
+    
+def binarySearch (arr, l, r, x): 
+  
+    if r >= l: 
+        mid = l + (r - l) // 2
+        if arr[mid] == x: 
+            return mid 
+        elif arr[mid] > x: 
+            return binarySearch(arr, l, mid-1, x) 
+        else: 
+            return binarySearch(arr, mid + 1, r, x) 
+    else: 
+        return -1
+    
 
-    return x_data,n
+print("                      HELLO!!")
+print("Welcome to the Number Plate Detection System.\n")
 
-#Main Program
+array=[]
+
 dir = os.path.dirname(__file__)
-x_data,n = Read(dir)
-print(x_data)
-sw = int(input("\n\n1) Sorting. \n2) Searching\nEnter : "))
 
-if sw == 1 :
-    Sort(x_data,n)
-    print("\nSorting Completed check the Sorted Folder")
-elif sw ==2 :
-    Search(x_data,dir)
-else:
-    print("\n\nInvalid Choice")
+for img in glob.glob(dir+"/Images/*.jpeg") :
+    img=cv2.imread(img)
+    
+    img2 = cv2.resize(img, (600, 600))
+    cv2.imshow("Image of car ",img2)
+    cv2.waitKey(1000)
+    cv2.destroyAllWindows()
+    
+    
+    number_plate=number_plate_detection(img)
+    res2 = str("".join(re.split("[^a-zA-Z0-9]*", number_plate)))
+    res2=res2.upper()
+    print(res2)
+
+    array.append(res2)
+
+array=quickSort(array,0,len(array)-1)
+print ("\n\n")
+print("The Vehicles numbers registered are:-")
+for i in array:
+    print(i)
+print ("\n\n")    
+
+for img in glob.glob(dir+"/search/*.jpeg") :
+    img=cv2.imread(img)
+    
+    number_plate=number_plate_detection(img)
+    res2 = str("".join(re.split("[^a-zA-Z0-9]*", number_plate)))
+
+
+print("The car number to search is:- ",res2)
+    
+
+
+result = binarySearch(array,0,len(array)-1,res2)
+if result != -1: 
+	print ("\n\nThe Vehicle is allowed to visit." ) 
+else: 
+    print ("\n\nThe Vehicle is  not allowed to visit.")
+        
+
+    			
